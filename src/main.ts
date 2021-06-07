6722b5ab10c56e76cc0091295e5a9b2797c83d78
Juraj Piar <juraj.piar@icloud.com>
2021-06-07 10:42:38 +0100

    1

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    console.log("Hello world")
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
